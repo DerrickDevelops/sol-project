e17b9a6e13b839953605ca4c581a4c6c271b3216
DerrickDevelops <derrickdevelops@gmail.com>
2024-06-06 21:51:59 -0400

    Updated Nav
    Added Navbar File

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import Navbar from './Navbar.jsx'
 
 function App() {
 
   return (
     <>
+      <Navbar />
       <h1>Sol System Website</h1>
     </>
   )
