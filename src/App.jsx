cbea2eb324c321ee35511e0ae9623d2f3cb2ab72
Derrick M. <163219068+DerrickDevelops@users.noreply.github.com>
2024-09-30 23:32:07 -0400

    Update App.jsx
    Add id names for each button + names (labels) for each button.

--- src/App.jsx
+++ src/App.jsx
@@ -10,14 +10,14 @@ function App() {
       <h1 className='header'>The Sol System</h1>
     <p className='sub-header'>Welcome, please select a planet.</p>
     <div className='planet-selection'>
-        <button id=''></button>
-        <button id=''></button>
-        <button id=''></button>
-        <button id=''></button>
-        <button id=''></button>
-        <button id=''></button>
-        <button id=''></button>
-        <button id=''></button>
+        <button id='mercury'>mercury</button>
+        <button id='venus'>venus</button>
+        <button id='earth'>earth</button>
+        <button id='mars'>mars</button>
+        <button id='jupiter'>jupiter</button>
+        <button id='saturn'>saturn</button>
+        <button id='uranus'>uranus</button>
+        <button id='neptune'>neptune</button>
         </div>
      </>
   )
