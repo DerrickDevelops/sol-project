b93a0b675123950dc883a603bb601915025b3149
Derrick M. <163219068+DerrickDevelops@users.noreply.github.com>
2024-10-07 23:20:04 -0400

    Update Navbar.jsx
    Added main code structure for navbar content.

---
+++ src/Navbar.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import Navbar from './Navbar.jsx'
+
+function Navbar() {
+
+  return (
+    <>
+      
+     </>
+  )
+}
+
+export default Navbar
