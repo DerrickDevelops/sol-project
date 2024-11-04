adc0ad128e08f89fa99c585a856ac72d4d304187
Derrick M. <163219068+DerrickDevelops@users.noreply.github.com>
2024-11-03 23:18:14 -0500

    Update Navbar.jsx
    Changed navbar links

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -9,8 +9,8 @@ function Navbar() {
     	<img src='' alt='logo' />
       <ul>
       	<li><a href=''>Planets</a></li>
+        <li><a href=''>Stars</a></li>
         <li><a href=''>Systems</a></li>
-        <li><a href=''>About Us</a></li>
         <li><a href=''>Contact</a></li>
       </ul>
      </>
